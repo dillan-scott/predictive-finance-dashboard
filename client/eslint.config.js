9ed17b0c6a21c39a1f0e3daf00143c8f4f504a3e
Dillan Scott <dillan.scott22@imperial.ac.uk>
2025-01-06 20:07:26 +0000

    feat: initial frontend installations and setup

---
+++ client/eslint.config.js
@@ -0,0 +1,27 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect", // Automatically detect the React version
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-empty-object-type": "warn",
+      "react/react-in-jsx-scope": "off", // React 17+ doesn't require React in scope
+      "react/jsx-uses-react": "off", // Prevents warnings about unused React import
+    },
+  },
+];
