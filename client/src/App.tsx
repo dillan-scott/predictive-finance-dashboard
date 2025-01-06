708c72b6f8cd719abfb7064a854c9e451abf4c00
Dillan Scott <dillan.scott22@imperial.ac.uk>
2025-01-06 20:28:16 +0000

    feat: configure material UI and setup theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,19 @@
+import { Box, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { useMemo } from "react";
+import { themeSettings } from "@/theme.ts";
+
 function App() {
-  return <div className="App"></div>;
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+          HELLO
+        </Box>
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
